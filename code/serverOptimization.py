0c2c98fbbdef590b0b7e94fc67836294c12eea2c
Tyler Hawkes <tyler.hawkes@vivint.com>
2018-03-11 21:37:27 -0600

    Added rack awareness and some more options for disk io and streaming

--- code/serverOptimization.py
+++ code/serverOptimization.py
@@ -45,7 +45,7 @@ class Cpu:
         baseVarName = "cpu {} {} @{}".format(family, cores, ghz)
         self.countVar = model.addVar(lb=0, vtype=GRB.INTEGER, name=baseVarName)
         self.useVar = model.addVar(vtype=GRB.BINARY, name="use " + baseVarName)
-        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctCpus")
+        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctCpus " + baseVarName)
 
 
 class Memory:
@@ -66,12 +66,13 @@ class Memory:
         baseVarName = "memory {}GB {}".format(self.memoryGB, self.type)
         self.countVar = model.addVar(lb=0, vtype=GRB.INTEGER, name=baseVarName)
         self.useVar = model.addVar(vtype=GRB.BINARY, name="use " + baseVarName)
-        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctMemory")
+        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctMemory " + baseVarName)
 
 
 class Disk:
     tb = 0
     iops = 0
+    streamMBs = 0
     type = "HDD"
     size = 2.5
     protocol = "SATA"
@@ -81,9 +82,19 @@ class Disk:
     countVar = None
     useVar = None
 
-    def __init__(self, model, tb, size, cost, iops, protocol, gbps, partnumber, disktype='HDD'):
+    def __init__(self, model, tb, size, cost, iops, protocol, gbps, partnumber, disktype=hdd, streammbs=0):
         self.tb = tb
         self.iops = iops
+        # Simplified generalizations
+        if streammbs == 0:
+            if disktype == hdd:
+                self.streamMBs = 200
+            if disktype == ssd:
+                self.streamMBs = 600
+            if disktype == nvme:
+                self.streamMBs = 1500
+        else:
+            self.streamMBs = streammbs
         self.type = disktype
         self.size = size
         self.protocol = protocol
@@ -93,7 +104,7 @@ class Disk:
         baseVarName = "disk {}TB {}\" {} {}".format(self.tb, self.size, self.protocol, self.partNumber)
         self.countVar = model.addVar(lb=0, vtype=GRB.INTEGER, name=baseVarName)
         self.useVar = model.addVar(vtype=GRB.BINARY, name="use " + baseVarName)
-        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctDisks")
+        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctDisks " + baseVarName)
 
 
 class Network:
@@ -114,7 +125,7 @@ class Network:
         baseVarName = "network {} x {} @ {} Gigabit by {}".format(adapterCount, adapterType, speed, manufacturer)
         self.countVar = model.addVar(lb=0, vtype=GRB.INTEGER, name=baseVarName)
         self.useVar = model.addVar(vtype=GRB.BINARY, name="use " + baseVarName)
-        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctNetworkCards")
+        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctNetworkCards " + baseVarName)
 
 
 class Server:
@@ -126,12 +137,9 @@ class Server:
     rackUnits = 1
     osDisks = 2
     osDiskType = 'dom'
-    # cpu = Cpu.default()
     cpuSlots = 2
-    # memory = Memory.default()
     memorySlots = 0
     minMemorySlots = 0
-    # disk = Disk.default()
     disk25Slots = 0
     disk35Slots = 0
     nvmeSlots = 0
@@ -145,6 +153,7 @@ class Server:
     configuredCost = 0
     countVar = None
     useVar = None
+    memoryVar = None
 
     def __init__(self, model, name, url, cost, watts, rackunits, cpuslots, memoryslots, minmemoryslots, disk25slots, disk35slots, nvmeslots, minimum25disks, minimum35disks, osdisks, osdisktype, x16, x8, onboardnetworkports, siomcards, powersupplies, nodes):
         self.name = name
@@ -174,13 +183,30 @@ class Server:
         baseVarName = "server " + self.name
         self.countVar = model.addVar(lb=0, vtype=GRB.INTEGER, name=baseVarName)
         self.useVar = model.addVar(vtype=GRB.BINARY, name="use " + baseVarName)
-        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctServers")
+        model.addConstr(self.countVar - 10000 * self.useVar, GRB.LESS_EQUAL, 0, "distinctServers " + baseVarName)
+        self.memoryVar = model.addVar(lb=0, ub=int(memoryslots / minmemoryslots), vtype=GRB.INTEGER, name="memory scalar " + baseVarName)
 
     def __str__(self):
         return self.name + ":" + str(self.configuredCost)
 
 
+class Rack:
+    availableRackUnits = 0
+    availableWatts = 0
+    networkCost = 0
+    costPerMonth = 0
+    timeHorizonYears = 0
+    useVar = None
+    rackUnits = LinExpr()
+    watts = LinExpr()
 
+    def __init__(self, model, name, availableRackUnits, availableWatts, networkCost, costPerMonth, timeHorizonYears):
+        self.availableRackUnits = availableRackUnits
+        self.availableWatts = availableWatts
+        self.networkCost = networkCost
+        self.costPerMonth = costPerMonth
+        self.timeHorizonYears = timeHorizonYears
+        self.useVar = model.addVar(vtype=GRB.BINARY, name="rack " + name)
 
 
 def buildVars(name):
@@ -217,22 +243,22 @@ def buildVars(name):
               Memory(m, 64, 'Load - Reduced', 2400, 999)]
 
     disk25 = [Disk(m, 5, 2.5, 213, 90, sata, 6, 'Barracuda5TB5400'),
-              # Disk(m, .3, 2.5, 189, 166, sas, 12, '0B28810'),
-              # Disk(m, .6, 2.5, 239, 166, sas, 12, '0B28808'),
-              # Disk(m, .9, 2.5, 309, 166, sas, 12, '0B27976'),
+              Disk(m, .3, 2.5, 189, 166, sas, 12, '0B28810'),
+              Disk(m, .6, 2.5, 239, 166, sas, 12, '0B28808'),
+              Disk(m, .9, 2.5, 309, 166, sas, 12, '0B27976'),
               Disk(m, 1.2, 2.5, 339, 166, sas, 12, '0B28807'),
-              # Disk(m, .3, 2.5, 269, 250, sas, 12, '0B28955'),
-              # Disk(m, .6, 2.5, 399, 250, sas, 12, '0B28953'),
+              Disk(m, .3, 2.5, 269, 250, sas, 12, '0B28955'),
+              Disk(m, .6, 2.5, 399, 250, sas, 12, '0B28953'),
               Disk(m, 1, 2.5, 209, 120, sata, 6, 'ST1000NX0313'),
               Disk(m, 2, 2.5, 349, 120, sata, 6, 'ST2000NX0253'),
               Disk(m, 1, 2.5, 229, 120, sas, 12, 'ST1000NX0333'),
               Disk(m, 2, 2.5, 389, 120, sas, 12, 'ST2000NX0273'),
-              # Disk(m, .6, 2.5, 189, 166, sas, 12, 'ST600MM0018'),
-              # Disk(m, .9, 2.5, 299, 166, sas, 12, 'ST900MM0018'),
+              Disk(m, .6, 2.5, 189, 166, sas, 12, 'ST600MM0018'),
+              Disk(m, .9, 2.5, 299, 166, sas, 12, 'ST900MM0018'),
               Disk(m, 1.2, 2.5, 339, 166, sas, 12, 'ST1200MM0018'),
               Disk(m, 1.8, 2.5, 439, 166, sas, 12, 'ST1800MM0018'),
-              # Disk(m, .3, 2.5, 249, 250, sas, 12, 'ST300MP0005'),
-              # Disk(m, .6, 2.5, 359, 250, sas, 12, 'ST600MP0005'),
+              Disk(m, .3, 2.5, 249, 250, sas, 12, 'ST300MP0005'),
+              Disk(m, .6, 2.5, 359, 250, sas, 12, 'ST600MP0005'),
               Disk(m, .24, 2.5, 149, 5000, sata, 6, 'Intel DC S4500 240', ssd),
               Disk(m, .48, 2.5, 259, 5000, sata, 6, 'Intel DC S4500 480', ssd),
               Disk(m, .96, 2.5, 489, 5000, sata, 6, 'Intel DC S4500 960', ssd),
@@ -284,8 +310,8 @@ def buildVars(name):
               Disk(m, 8, 3.5, 329, 120, sas, 12, 'ST8000NM0075'),
               Disk(m, 10, 3.5, 449, 120, sas, 12, 'ST10000NM0096'),
               Disk(m, 12, 3.5, 529, 120, sas, 12, 'ST12000NM0027'),
-              # Disk(m, 1, 3.5, 209, 120, sata, 6, 'ST1000NX0313'),
-              # Disk(m, 2, 3.5, 349, 120, sata, 6, 'ST2000NX0253'),
+              Disk(m, 1, 3.5, 209, 120, sata, 6, 'ST1000NX0313'),
+              Disk(m, 2, 3.5, 349, 120, sata, 6, 'ST2000NX0253'),
              ]
 
     diskNvme = [Disk(m, 2, 1.5, 1149, 20000, nvme, 24, 'Intel P3520 NVMe', nvme),
@@ -356,16 +382,19 @@ def buildVars(name):
                Server(m, "16x2.5 2 Node", "https://www.thinkmate.com/product/supermicro/sys-2028tp-dttr", 2632, 1280, 2, 2, 16, 8, 8, 0, 0, 0, 0, 1, dom, 1, 1, 2, 0, 2, 2),
                ]
 
-    return m, cpus, memory, disk25, disk35, diskNvme, siom, pciNetwork, servers
+    racks = [Rack(m, "1", 37, availableWattsPerRack, 35000, rackCostPerMonth, serverCostTimeHorizonYears),
+             Rack(m, "2", 37, availableWattsPerRack, 35000, rackCostPerMonth, serverCostTimeHorizonYears),
+             Rack(m, "3", 38, availableWattsPerRack, 20000, rackCostPerMonth, serverCostTimeHorizonYears),]
+
+    return m, cpus, memory, disk25, disk35, diskNvme, siom, pciNetwork, servers, racks
 
 
 def ilp():
-    m, cpus, memory, disk25, disk35, diskNvme, siom, pciNetwork, servers = buildVars("ILP")
+    m, cpus, memory, disk25, disk35, diskNvme, siom, pciNetwork, servers, racks = buildVars("ILP")
 
-    #TODO add power, cooling and rack space to costs
+    cost = LinExpr()
 
     ##### Server #####
-    cost = LinExpr()
     serverCount = LinExpr()
     nodeCount = LinExpr()
     serverTypes = LinExpr()
@@ -374,6 +403,8 @@ def ilp():
     serverCpuSlots = LinExpr()
     serverMemorySlots = LinExpr()
     serverMinMemorySlots = LinExpr()
+    serverMemoryBundles = LinExpr()
+    serverMinMemoryBundles = LinExpr()
     server25DiskSlots = LinExpr()
     server35DiskSlots = LinExpr()
     serverNvmeSlots = LinExpr()
@@ -391,6 +422,8 @@ def ilp():
         serverCpuSlots = serverCpuSlots + server.countVar * (server.cpuSlots * server.nodes)
         serverMemorySlots = serverMemorySlots + server.countVar * (server.memorySlots * server.nodes)
         serverMinMemorySlots = serverMinMemorySlots + server.countVar * (server.minMemorySlots * server.nodes)
+        serverMemoryBundles = serverMemoryBundles + server.countVar * (server.memorySlots / server.minMemorySlots * server.nodes)
+        serverMinMemoryBundles = serverMinMemoryBundles + server.countVar * (1 * server.nodes)  # server.minMemorySlots / server.minMemorySlot = 1
         server25DiskSlots = server25DiskSlots + server.countVar * (server.disk25Slots * server.nodes)
         min25Disks = min25Disks + server.countVar * (server.minimum25Disks * server.nodes)
         server35DiskSlots = server35DiskSlots + server.countVar * (server.disk35Slots * server.nodes)
@@ -402,6 +435,20 @@ def ilp():
     m.addConstr(serverCount, GRB.GREATER_EQUAL, minServers, "minServers")
     m.addConstr(serverCount, GRB.LESS_EQUAL, maxServers, "maxServers")
     m.addConstr(serverTypes, GRB.LESS_EQUAL, maxDistinctServerTypes, "maxDistinctServerTypes")
+
+    ##### Racks #####
+
+    maxRackUnits = LinExpr()
+    maxWatts = LinExpr()
+    previousRack = None
+    for rack in racks:
+        if previousRack != None:
+            m.addConstr(rack.useVar, GRB.LESS_EQUAL, previousRack.useVar, "rack {} after rack {}".format(rack, previousRack))
+        previousRack = rack
+        maxRackUnits = maxRackUnits + rack.useVar * rack.availableRackUnits
+        maxWatts = maxWatts + rack.useVar * rack.availableWatts
+        cost = cost + rack.useVar * rackCostScalingFactor * (rack.networkCost + (rack.costPerMonth * 12 * rack.timeHorizonYears))
+
     m.addConstr(rackUnits, GRB.LESS_EQUAL, maxRackUnits, "maxRackUnits")
     m.addConstr(watts, GRB.LESS_EQUAL, maxWatts, "maxWatts")
 
@@ -436,12 +483,11 @@ def ilp():
     m.addConstr(memoryGB, GRB.GREATER_EQUAL, minMemory, "minMemory")
     m.addConstr(memorySlots, GRB.LESS_EQUAL, serverMemorySlots, "maxMemorySlotsAvailable")
     m.addConstr(memorySlots, GRB.GREATER_EQUAL, serverMinMemorySlots, "minMemorySlotsRequiredByServers")
+    #m.addConstr(memorySlots, GRB.LESS_EQUAL, serverMemoryBundles)
     m.addConstr(nodeCount * maxAvgMemoryPerNode - memoryGB, GRB.GREATER_EQUAL, 0, "maxAvgMemoryPerNode")
     m.addConstr(nodeCount * minAvgMemoryPerNode - memoryGB, GRB.LESS_EQUAL, 0, "minAvgMemoryPerNode")
 
-
     ##### Disk #####
-
     allDisks = []
     allDisks.extend(disk25)
     allDisks.extend(disk35)
@@ -452,10 +498,16 @@ def ilp():
     diskTypes35 = LinExpr()
     diskTypesNvme = LinExpr()
     diskTB = LinExpr()
+    diskHddTB = LinExpr()
+    diskFastTB = LinExpr()
     diskIOPS = LinExpr()
+    diskHddIOPS = LinExpr()
+    diskFastIOPS = LinExpr()
+    diskStreamMBs = LinExpr()
     disk25Count = LinExpr()
     disk35Count = LinExpr()
     diskNvmeCount = LinExpr()
+
     for disk in disk25:
         disk25Count = disk25Count + disk.countVar
         diskTypes25 = diskTypes25 + disk.useVar
@@ -470,11 +522,27 @@ def ilp():
         diskTypes = diskTypes + disk.useVar
         diskTB = diskTB + disk.countVar * disk.tb
         diskIOPS = diskIOPS + disk.countVar * disk.iops
+        diskStreamMBs = diskStreamMBs + disk.countVar * disk.streamMBs
         cost = cost + disk.countVar * disk.cost
+        if disk.type == hdd:
+            diskHddTB = diskHddTB + disk.countVar * disk.tb
+            diskHddIOPS = diskHddIOPS + disk.countVar * disk.iops
+        else:
+            diskFastTB = diskFastTB + disk.countVar * disk.tb
+            diskFastIOPS = diskFastIOPS + disk.countVar * disk.iops
 
     m.addConstr(diskTypes, GRB.LESS_EQUAL, maxDistinctDisks, "distinctDiskTypes")
+    m.addConstr(diskTypes25, GRB.LESS_EQUAL, maxDistinct25Disks, "maxDistinct25Disks")
+    m.addConstr(diskTypes35, GRB.LESS_EQUAL, maxDistinct35Disks, "maxDistinct35Disks")
+    m.addConstr(diskTypesNvme, GRB.LESS_EQUAL, maxDistinctNvmeDisks, "maxDistinctNvmeDisks")
     m.addConstr(diskTB, GRB.GREATER_EQUAL, minDiskTB, "minimumTerabytes")
+    m.addConstr(diskHddTB, GRB.GREATER_EQUAL, minDiskHddTB, "minimumHddTB")
+    m.addConstr(minDiskFastTB, GRB.GREATER_EQUAL, minDiskFastTB, "minimumFastTB")
     m.addConstr(diskIOPS, GRB.GREATER_EQUAL, minDiskIOPS, "minimumIOPS")
+    m.addConstr(diskHddIOPS, GRB.GREATER_EQUAL, minDiskHddIOPS, "minimumHddIOPS")
+    m.addConstr(diskFastIOPS, GRB.GREATER_EQUAL, minDiskFastIOPS, "minimumFastIOPS")
+    m.addConstr(diskStreamMBs, GRB.GREATER_EQUAL, minDiskStreamMBs, "minimumStreamMBs")
+    m.addConstr(disk25Count + disk35Count + diskNvmeCount, GRB.GREATER_EQUAL, minDiskCount, "minDiskCount")
     if fillAllDiskSlots:
         m.addConstr(disk35Count, GRB.EQUAL, server35DiskSlots, "fillAll35DiskSlots")
         m.addConstr(disk25Count, GRB.EQUAL, server25DiskSlots, "fillAll25DiskSlots")
@@ -484,6 +552,7 @@ def ilp():
         m.addConstr(disk25Count, GRB.GREATER_EQUAL, min25Disks, "min25DiskRequirement")
         m.addConstr(disk25Count, GRB.LESS_EQUAL, server25DiskSlots, "max25DiskSlots")
 
+    ##### Network #####
     networks = []
     networks.extend(siom)
     networks.extend(pciNetwork)
@@ -525,54 +594,61 @@ def ilp():
 
     for v in m.getVars():
         if abs(v.X) > 1e-6 and not v.varName.startswith("use "):
-            print (v.varName, v.X)
+            print (v.varName, int(round(v.X, 0)))
     return m.getAttr("ObjVal")
 
 
 ##### inputs #####
 
+# Servers
 maxDistinctServerTypes = 1
 minServers = 0
-maxServers = 10000
-maxRackUnits = 200
-maxWatts = 999999
-minMemory = 8000
+maxServers = 18
+
+# Memory
 maxDistinctMemory = 1
-maxAvgMemoryPerNode = 1000
+minMemory = 12288
+maxAvgMemoryPerNode = 3000
 minAvgMemoryPerNode = 256
+
+# Cpus
+maxDistinctCpus = 2
 minCores = 0
-minGigaflops = 2000
-maxDistinctCpus = 1
-minDiskTB = 1800
-minDiskIOPS = 40000
-maxDistinctDisks = 1
-fillAllDiskSlots = False
-minTotalNetworkSpeedGigabits = 6000
+minGigaflops = 700
+
+# Disk
+maxDistinctDisks = 2
+maxDistinct25Disks = 2
+maxDistinct35Disks = 1
+maxDistinctNvmeDisks = 0
+minDiskCount = 720
+minDiskTB = 3600
+minDiskIOPS = 0
+minDiskStreamMBs = 12000
+minDiskHddIOPS = 30000
+minDiskHddTB = 600
+minDiskFastIOPS = 0
+minDiskFastTB = 300
+fillAllDiskSlots = True
+
+# Netowrk
+minTotalNetworkSpeedGigabits = 3000
 minNetworkConnectionsPerNode = 2
 maxNetworkConnections = 10000
 maxDistinctNetworkCards = 1
 minNetworkCardsPerServer = 1
 maxNetworkCardsPerServer = 1
 allowSiomCards = False
-maxAvailableRacks = 3
-rackunitsPerRack = 37
-rackCostPerMonth = 0
+
+# Racks
+rackCostPerMonth = 2000
 serverCostTimeHorizonYears = 3
-maxWattsPerRack = 999999
+availableWattsPerRack = 999999
+# Used to shut off rack costs
+rackCostScalingFactor = 0.000001
+
 
 bestIlp = ilp()
 print(bestIlp)
 
 
-#target
-#cost
-#cpu
-#memory
-#disktb
-#diskiops
-
-
-#cost influenced by
-#rackunits
-#watts
-#network ports
